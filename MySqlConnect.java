150a0a55bb3c66c9bad91d848df370c7247bf1a5
Mark Jun A. Gersaniva <markjungersaniva@gmail.com>
2016-05-06 11:46:45 +0700

    Create MySqlConnect.java

---
+++ MySqlConnect.java
@@ -0,0 +1,25 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+import java.sql.*;
+import javax.swing.*;
+/**
+ *
+ * @author nujkram
+ */
+public class MySqlConnect {
+    Connection myConn = null;
+    public static Connection ConnectDB() {
+        try{
+            Class.forName("com.mysql.jdbc.Driver");
+            Connection myConn = DriverManager.getConnection("jdbc:mysql://localhost:3306/dbrecords", "root","admin");
+            JOptionPane.showMessageDialog(null, "Database Connected");
+            return myConn;
+        } catch (Exception exc) {
+            JOptionPane.showMessageDialog(null, exc);
+            return null;
+        }
+    }
+}
